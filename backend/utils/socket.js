498910767cf73dbbcfd7db9c3c511893701636f6
Udit Pandey <142110719+uditc0derr@users.noreply.github.com>
2025-03-31 13:12:06 +0530

    Update socket.js

--- backend/utils/socket.js
+++ backend/utils/socket.js
@@ -3,45 +3,40 @@ const { Server } = require("socket.io");
 const initializeSocket = (server) => {
   const io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173", // Your React frontend URL
+      origin: "http://localhost:5173", 
       methods: ["GET", "POST"],
       allowedHeaders: ["Authorization"],
     },
   });
 
-  let users = {}; // Store connected users
+  let users = {}; 
 
   io.on("connection", (socket) => {
     console.log(`User connected: ${socket.id}`);
 
-    // Store user ID when they join
     socket.on("join", ({ userId }) => {
       users[userId] = socket.id;
       console.log(`User joined: ${userId}`);
     });
 
-    // Handle blood request
     socket.on("sendRequest", ({ requesterId, donorId, requestData }) => {
       if (users[donorId]) {
         io.to(users[donorId]).emit("receiveRequest", { requesterId, requestData });
       }
     });
 
-    // Accept request
     socket.on("acceptRequest", ({ donorId, requesterId, response }) => {
       if (users[requesterId]) {
         io.to(users[requesterId]).emit("requestAccepted", { donorId, response });
       }
     });
 
-    // Handle real-time chat
     socket.on("sendMessage", ({ senderId, receiverId, message }) => {
       if (users[receiverId]) {
         io.to(users[receiverId]).emit("receiveMessage", { senderId, message });
       }
     });
 
-    // Handle disconnection
     socket.on("disconnect", () => {
       Object.keys(users).forEach((userId) => {
         if (users[userId] === socket.id) {
